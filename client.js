04f2a18a7a3144aee278391bcad99cdf00245d9c
Waldyn Benbenek <wben1201@gmail.com>
2018-12-14 16:57:40 -0600

    everything but the erase function

--- client.js
+++ client.js
@@ -1,25 +1,25 @@
 $(document).ready(readyNow);
-class Employee{
-    constructor(firstNameIn, lastNameIn, idIn, titleIn, aSalaryIn){
-    this.fname = firstNameIn
-    this.lname = lastNameIn
-    this.id = idIn
-    this.title = titleIn
-    this.asalary = aSalaryIn
-}
+class Employee {
+    constructor(firstNameIn, lastNameIn, idIn, titleIn, aSalaryIn) {
+        this.fname = firstNameIn
+        this.lname = lastNameIn
+        this.id = idIn
+        this.title = titleIn
+        this.asalary = aSalaryIn
+    }
 }
 const employees = []
 let finalSalary = 0
-function readyNow(){
-   
-   
+function readyNow() {
+
+
     $('h1').after('<input type="text" id="firstName" placeholder="First Name">');
     $('#firstName').after('<input type="text" id="lastName" placeholder="Last Name">');
     $('#lastName').after('<input type="number" id="empId" placeholder="Emloyee Id">');
     $('#empId').after('<input type="text" id="empTitle" placeholder="Emloyee Title">');
     $('#empTitle').after('<input type="number" id="aSalary" placeholder="Annual Salary">');
-    
-    
+
+
     $('#aSalary').after('<button id="addEmployee"> Add Employee </button>');
     $('#addEmployee').on('click', function newEmp() {
         let firstN = $('#firstName').val();
@@ -28,19 +28,26 @@ function readyNow(){
         let employeeTitle = $('#empTitle').val()
         let annualSalary = $('#aSalary').val()
         employees.push(new Employee(firstN, lastN, employeeId, employeeTitle, annualSalary));
-        $('ul').append('<li>'+ firstN + lastN + employeeId + employeeTitle + annualSalary + '</li>')
-       
+        $('ul').append('<li>' + firstN + " " + lastN + " " + employeeId + " " + employeeTitle + " " + annualSalary + '</li>')
+        aCalc()
+        yeetSalary();
     });
 
 }
 
 
-function aCalc(){
-    for (i = 0; i < employees.length; i++){
-let monthlySalary = employees[i].asalary / 12
-finalSalary += monthlySalary
-console.log(finalSalary)
-
+function aCalc() {
+    for (i = 0; i < employees.length; i++) {
+        let monthlySalary = employees[i].asalary / 12
+        finalSalary += monthlySalary
+        console.log(finalSalary)
     }
+    $('h2').remove()
+    $('ul').after('<h2 id="yeetedS">' + finalSalary + '</h2>')
 }
 
+function yeetSalary() {
+    if (finalSalary > 20000) {
+        $('h2').css('color','red');// getter & setter
+    }
+}
